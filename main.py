d302b82f238a4d92eea437f1f21c9c5764bdcbd2
SaPradilla <santypradilla1985@gmail.com>
2024-11-21 17:23:31 -0500

    ft: update mapping

--- main.py
+++ main.py
@@ -1,62 +1,82 @@
 import ijson
 import pandas as pd
+import os
+from tqdm import tqdm
 
-with open('archivo.json', 'r', encoding='utf-8') as file:
+# Create the 'conversations' folder if it does not exist
+output_directory = 'conversations'
+if not os.path.exists(output_directory):
+    os.makedirs(output_directory)
+# Count the total number of messages in the JSON file
+message_count = 0
+with open('data.json', 'r', encoding='utf-8') as file:
+    parser = ijson.parse(file)
+    for prefix, event, value in parser:
+        if prefix.endswith('.MessageList.item') and event == 'end_map':
+            message_count += 1
+
+# Open json file
+with open('data.json', 'r', encoding='utf-8') as file:
     parser = ijson.parse(file)
     
-    # Variables para almacenar datos temporales
+    # temp vars
     message_data = {}
     current_field = None
     messages = []
-     # Número de mensajes por bloque
-    batch_size = 10000 
+    # Number of messages per block
+    batch_size = 10000
     batch_number = 0
     total_messages = 0
 
-    print("Iniciando el procesamiento del archivo JSON...")
+    print("Starting JSON file processing ^_^")
 
-    # Recorre el parser
-    for prefix, event, value in parser:
-        if prefix.endswith('.MessageList.item') and event == 'start_map':
-            message_data = {}
+    # Initial bar progress
+    with tqdm(total=message_count, desc="Processing messages") as pbar:
+        for prefix, event, value in parser:
+            if prefix.endswith('.MessageList.item') and event == 'start_map':
+                message_data = {}
 
-        if prefix.endswith('.MessageList.item') and event == 'end_map':
-            messages.append(message_data)
-            total_messages += 1
+            if prefix.endswith('.MessageList.item') and event == 'end_map':
+                messages.append(message_data)
+                total_messages += 1
+                 
+                # Updates the progress bar
+                pbar.update(1) 
 
-            # Si alcanzamos el tamaño del bloque, procesamos y guardamos
-            if len(messages) == batch_size:
-                print(f"Procesando bloque {batch_number + 1}, mensajes totales procesados: {total_messages}")
-                df = pd.DataFrame(messages)
-                df.to_excel(f'conversaciones_{batch_number}.xlsx', index=False)
-                print(f"Bloque {batch_number + 1} guardado como 'conversaciones_{batch_number}.xlsx'")
-                batch_number += 1
-                messages = []
+                
+                # If we reach the block size, we process and save
 
-        if event == 'map_key':
-            current_field = value
-        elif current_field == 'id' and event == 'number':
-            message_data['id'] = value
-        elif current_field == 'displayName' and event == 'string':
-            message_data['displayName'] = value
-        elif current_field == 'originalarrivaltim' and event == 'string':
-            message_data['originalarrivaltim'] = value
-        elif current_field == 'messagetype' and event == 'string':
-            message_data['messagetype'] = value
-        elif current_field == 'version' and event == 'number':
-            message_data['version'] = value
-        elif current_field == 'content' and event == 'string':
-            message_data['content'] = value
-        elif current_field == 'conversationid' and event == 'string':
-            message_data['conversationid'] = value
-        elif current_field == 'from' and event == 'string':
-            message_data['from'] = value
+                if len(messages) == batch_size:
+                    df = pd.DataFrame(messages)
+                    try:
+                        output_path = os.path.join(output_directory, f'conversations_{batch_number}.xlsx')
+                        df.to_excel(output_path, index=False)
+                    except Exception as e:
+                        print(f"Error saving the file: {e}")
+                    batch_number += 1
+                    messages = []
 
-    # Procesa cualquier mensaje restante
-    if messages:
-        print(f"Procesando último bloque, mensajes totales procesados: {total_messages}")
-        df = pd.DataFrame(messages)
-        df.to_excel(f'conversaciones_{batch_number}.xlsx', index=False)
-        print(f"Último bloque guardado como 'conversaciones_{batch_number}.xlsx'")
+            if event == 'map_key':
+                current_field = value
 
-print("Procesamiento completado.")
+            elif current_field == 'displayName' and event == 'string':
+                message_data['name'] = value
+            elif current_field == 'messagetype' and event == 'string':
+                message_data['messagetype'] = value
+            elif current_field == 'content' and event == 'string':
+                message_data['message'] = value
+            elif current_field == 'from' and event == 'string':
+                message_data['from'] = value
+            elif current_field == 'originalarrivaltime' and event == 'string':
+                message_data['date'] = pd.to_datetime(value).tz_localize(None)
+
+        # Process any remaining messages
+        if messages:
+            df = pd.DataFrame(messages)
+            try:
+                output_path = os.path.join(output_directory, f'conversations_{batch_number}.xlsx')
+                df.to_excel(output_path, index=False)
+            except Exception as e:
+                print(f"Error saving the file: {e}")
+
+print("Processing completed :-D")
